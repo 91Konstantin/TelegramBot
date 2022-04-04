678744da00af51ce39eaece515e917335b026c99
91Konstantin <92858725+91Konstantin@users.noreply.github.com>
2022-04-04 22:04:23 +0300

    ok

--- bot.py
+++ bot.py
@@ -3,27 +3,8 @@ import idsList #Авторизованные
 import telebot
 from telebot import types
 
-#Для поиска последнего файла в папке
-import os
-import time
-
-def CheckFileTime(path, TimeDeltaMax):
-    files = os.listdir(path) #Все файлы в директории
-    files = [os.path.join(path, file) for file in files] #Превратим просто список файлов в список файлов с путями
-    files = [file for file in files if os.path.isfile(file)] #Оставляем в списке только файлы
-    MaxTimeFile = max(files, key=os.path.getctime) #Путь к файлу с максимальной датой
-    TimeDelta = time.time() - os.path.getmtime(MaxTimeFile) #Вычисляем разницу во времени создания сайта и текущей в секундах
-    if TimeDelta > TimeDeltaMax:
-        return 1
-    else:
-        return 0
-
-def GetMaxTimeFilePath(path):
-    files = os.listdir(path) #Все файлы в директории
-    files = [os.path.join(path, file) for file in files] #Превратим просто список файлов в список файлов с путями
-    files = [file for file in files if os.path.isfile(file)] #Оставляем в списке только файлы
-    MaxTimeFilePath = max(files, key=os.path.getctime) #Путь к файлу с максимальной датой
-    return MaxTimeFilePath
+#Для вызова данных ЦБ
+import GetCBdata
 
 #Для логирования
 import logging
@@ -48,7 +29,7 @@ bot = telebot.TeleBot(config.TOKEN)
 def start_handler(message):
     logging.info(LogMessage(message))
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=1)
-    btn1 = types.KeyboardButton(text='EOL и кадидаты')
+    btn1 = types.KeyboardButton(text='ЦБ РФ')
     kb.add(btn1)
     if message.from_user.id not in idsList.ids:
         bot.send_message(message.chat.id, str('ID пользователя НЕ в списке разрешённых. ID:') + str(message.from_user.id))
@@ -65,21 +46,33 @@ def some(message):
 @bot.message_handler(content_types=['text'])
 def button_handler(message):
     logging.info(LogMessage(message))
-    if message.text == 'EOL и кадидаты':
-        bot.send_message(message.chat.id, 'Ушёл считать. Жди:)')
-        FileCheck = CheckFileTime(config.EOLPath, config.TimeDeltaMax)
-        if FileCheck ==0:
-            doc = open(GetMaxTimeFilePath(config.EOLPath), 'rb')
-            bot.send_document(message.chat.id, doc)
-        if FileCheck ==1:
-            bot.send_message(message.chat.id, 'Тут будет функция создания нового файла') #Вставить функцию
-            doc = open(GetMaxTimeFilePath(config.EOLPath), 'rb')
-            bot.send_document(message.chat.id, doc)
-        else:
-            pass
-    else:
-        bot.send_message(message.chat.id, 'Нет такой команды')
+    
+    if message.text == 'ЦБ РФ':
+        try:
+            CurString = GetCBdata.GetCurString()
+            bot.send_message(message.chat.id, CurString[0])
+            bot.send_message(message.chat.id, CurString[1])
+            bot.send_message(message.chat.id, CurString[2])
+            bot.send_message(message.chat.id, CurString[3])
+        except Exception: 
+            bot.send_message(message.chat.id,'Что-то пошло не так...')
+            logging.info(LogMessage(message) + str(' Fail'))
 
+    #Отправка лога по запросу администратора
+    elif (message.chat.id in idsList.Adminids) & (message.text == 'лог'):
+        try:
+            logging.info(LogMessage(message))
+            doc = open(config.LogPath, 'rb')
+            bot.send_document(message.chat.id, doc)
+        except Exception: 
+            bot.send_message(message.chat.id,'Что-то пошло не так...')
+            logging.info(LogMessage(message) + str(' Fail'))
+
+    else:
+        kb = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=1)
+        btn1 = types.KeyboardButton(text='ЦБ РФ')
+        kb.add(btn1)
+        bot.send_message(message.chat.id, 'Нет такой команды', reply_markup=kb)
 
 #Цикл
 bot.polling(none_stop=True)
